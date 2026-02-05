12d686519e86eb318daa1a5866447ec9bd92b591
Saida <saidarozo03@gmail.com>
2026-02-05 16:39:43 -0500

    correción main

---
+++ Calculadora.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Gadiel Mejia, Saida Rozo, Jhon Hernandez, David Vergara, Alejandro 
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        //IMC
+        
+        
+        
+        
+        
+        
+        
+        //PGC
+        
+        
+        
+        
+        
+        
+        
+        //Masa Metabolica Basal
+        
+        
+        
+        
+        
+        
+        
+        //Calculo calorias
+        
+        
+       
+        
+        
+    }
+}
